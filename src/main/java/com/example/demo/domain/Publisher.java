095cd8f4b065317fd24ee625885725a2c8a67a24
Toader, Robert <toader.robert.cntv@gmail.com>
2021-08-10 02:00:52 +0300

    added books to publisher

--- src/main/java/com/example/demo/domain/Publisher.java
+++ src/main/java/com/example/demo/domain/Publisher.java
@@ -1,9 +1,8 @@
 package com.example.demo.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -18,6 +17,10 @@ public class Publisher {
     private String state;
     private String zip;
 
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
+
     public Publisher() {
     }
 
@@ -31,6 +34,14 @@ public class Publisher {
                 ", state='" + state + '\'' +
                 ", zip='" + zip + '\'' +
                 '}';
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     @Override
